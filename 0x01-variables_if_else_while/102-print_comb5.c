a89199a3d1649d4c4208a61196f016f3dbe7f14a
kiki-marge <keithfreeman762@gmail.com>
2022-09-12 14:20:47 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: Prints all possible combinations 
+ * of two-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int a;
+
+	while (i <= 99)
+	{
+		a = i;
+		while (a <= 99)
+		{
+			if (a != i)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+
+				if (i != 98 || a != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++a;
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
