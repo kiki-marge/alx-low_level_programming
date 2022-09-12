c1890b08881cfd0d922077ce9b5c5954aed592f4
kiki-marge <keithfreeman762@gmail.com>
2022-09-12 11:51:26 -0700

    4 Alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/***
+ * main - main block
+ * Description: Print all the alphabet in lowercase
+ * except q and e
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
