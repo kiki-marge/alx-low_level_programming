7425f2f997026fd9b439e00ac07dc5f4a1932952
kiki-marge <keithfreeman762@gmail.com>
2022-09-15 14:00:52 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[10] = "_putchar";
+	int i = 0;
+	while (i <9)
+	{
+	_putchar(c[i]);
+	i++;
+	}
 
+	_putchar('\n');
 	return (0);
 }
