e691d4dc248357a8accc32b7b3a9d6cc7436ae80
kiki-marge <keithfreeman762@gmail.com>
2022-09-16 13:31:08 -0700

    1-Main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+	
+	i=0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}			
