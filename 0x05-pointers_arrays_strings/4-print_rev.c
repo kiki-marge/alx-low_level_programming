22149e6f6964b770f6608a0bf7c3a89cebe75360
kiki-marge <keithfreeman762@gmail.com>
2022-09-20 14:33:48 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (index = 0; s[i] != '\0'; ++i);
+	for (i = 0; s[i] != '\0'; ++i);
 
-	for (--i; i>= 0; --i)
+	for (--i; i >= 0; --i)
 		_putchar(s[i]);
 	_putchar('\n');
 }
